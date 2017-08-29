b364baa760edbee1b633dbe07c7fa3fa7559af43
arikui1911 <arikui.ruby@gmail.com>
2017-08-29 17:33:14 +0900

    add some test

--- test/test_raijin.rb
+++ test/test_raijin.rb
@@ -1,5 +1,15 @@
+gem 'test-unit'
 require 'test/unit'
 require 'raijin'
 
 class TestRaijin < Test::Unit::TestCase
+  sub_test_case 'default private methods' do
+    setup do
+      @cli = Class.new(Raijin).new
+    end
+
+    test '#command_nothing' do
+      assert_raise(Raijin::Error){ @cli.__send__ :command_nothing }
+    end
+  end
 end
